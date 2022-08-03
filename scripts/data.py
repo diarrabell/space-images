194eae148440ef5259516559d1df2a3e95a009e5
Diarra Bell <dbell@6river.com>
2022-08-03 06:49:16 -0400

    adding files

---
+++ scripts/data.py
@@ -0,0 +1,22 @@
+"""
+This script cleans the data and prepares it for modeling
+"""
+
+import os
+import pandas as pd
+import numpy as np
+from sklearn.utils import shuffle
+
+class ExtractData:
+    def __init__(self) -> None:
+        pass
+
+class Dataframe:
+    def __init__(self, zipfile_name) -> None:
+        self.zipfile_name = zipfile_name
+
+    def extract_data(self):
+        print ("extracting")
+
+
+
---
+++ scripts/dataloader.py
@@ -0,0 +1,9 @@
+"""
+Script for creating dataloaders
+"""
+
+class Dataloader:
+    def __init__(self) -> None:
+        pass
+
+     
